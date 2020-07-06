66019e9962028ead038b45beee91604d4f2059cc
emmacps <emmacps001@gmail.com>
2020-07-06 03:15:57 +0000

    final update

---
+++ js/style.js
@@ -0,0 +1,15 @@
+alert("Welcome to ENT Events Management Services...");
+
+
+function contact() {
+    confirm("Please confirm yor appointment");
+}
+
+function myAppoint() {
+    var name = prompt("Enter your name");
+    var num = prompt("Enter your mobile number");
+    var email = prompt("Enter your email");
+
+    document.write("Your appointment details" + "<br>" + name + "<br>" + "<br>" + num + "<br>" + email);
+
+}
